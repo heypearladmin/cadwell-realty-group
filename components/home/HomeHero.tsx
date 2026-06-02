11a09713e0ee4953ce426db4b494354dac7efef1
Dale Gatchallan <dale@heypearl.io>
2026-06-03 00:23:49 +0800

    Tune hero image position to object-[center_15%] for balanced landscape

--- components/home/HomeHero.tsx
+++ components/home/HomeHero.tsx
@@ -44,7 +44,7 @@ export function HomeHero() {
             fill
             priority
             sizes="100vw"
-            className="object-cover object-top"
+            className="object-cover object-[center_15%]"
           />
         </div>
         <div className="ken-burns absolute inset-0 md:hidden">
@@ -54,7 +54,7 @@ export function HomeHero() {
             fill
             priority
             sizes="100vw"
-            className="object-cover object-top"
+            className="object-cover object-[center_15%]"
           />
         </div>
         {/* Layered scrims */}
